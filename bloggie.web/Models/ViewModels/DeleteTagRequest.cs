48647059fb58dd1df8c04ab653c51864a570754e
Mohammad_Ebrh <mohammad.s13811381@gmail.com>
2024-04-21 10:15:44 +0330

    Adding DeleteTagRequest.cs

---
+++ bloggie.web/Models/ViewModels/DeleteTagRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace bloggie.web.Models.ViewModels;
+
+public class DeleteTagRequest
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+}
