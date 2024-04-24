f5214a95e5b53c9d32e94221b0b0016c5addcd06
Mohammad_Ebrh <mohammad.s13811381@gmail.com>
2024-04-24 12:00:29 +0330

    Adding Blog controller, AddBlogPostRequest.cs

---
+++ bloggie.web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bloggie.web.Controllers;
+
+public class AdminBlogPostsController : Controller
+{
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+}
