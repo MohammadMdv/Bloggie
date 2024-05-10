9e64137e49e02e2451d4dd257d52d53bd1a59501
Mohammad_Ebrh <mohammad.s13811381@gmail.com>
2024-05-10 19:11:44 +0330

    Adding SignUp page and AccountController.cs

---
+++ bloggie.web/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using bloggie.web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bloggie.web.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    // GET
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(registerViewModel);
+        }
+
+        var user = new IdentityUser
+        {
+            UserName = registerViewModel.Username,
+            Email = registerViewModel.Email
+        };
+
+        var identityResult = await _userManager.CreateAsync(user, registerViewModel.Password);
+
+        if (identityResult.Succeeded)
+        {
+            var roleIdentityResult = await _userManager.AddToRoleAsync(user, "User");
+            if (roleIdentityResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        
+        return View(); 
+    }
+}
